978d0bca10b1a463aac95f25a75ae76b437c867f
numi8462 <yhk8462@naver.com>
2025-01-20 17:56:33 +0900

    Update pccp2.js

--- pccp2.js
+++ pccp2.js
@@ -1,30 +1,39 @@
 function solution(diffs, times, limit) {
-	// 이진탐색
+    let right = Math.max(...diffs);
     let left = 1;
-    let right = 100000;
-
-    while (left < right) {
-        let mid = Math.floor((left + right) / 2);
+    
+    while(left < right){
         let totalTime = 0;
-        let currLvl = mid;
-
-        for (let i = 0; i < diffs.length; i++) {
-            if (currLvl >= diffs[i]) {
-                totalTime += times[i];
+        let mid = Math.floor((left + right) / 2);
+        let level = mid;
+        console.log('level: ' + level)
+        for(let i = 0; i < diffs.length; i++){
+            // if first question prevTime = 0;
+            if(i === 0){
+                prevTime = 0
             } else {
-                let wrongAmount = diffs[i] - currLvl;
-                let prevTime = i === 0 ? times[i] : times[i - 1] + times[i];
-                totalTime += wrongAmount * prevTime + times[i];
+                prevTime = times[i - 1];
             }
 
-            if (totalTime > limit) {
+            if(level >= diffs[i]){
+                totalTime += times[i];
+            } else {
+                totalTime += (times[i] + prevTime) * (diffs[i] - level) + times[i];
+            }
+
+            // console.log('diff: ' + diffs[i])
+            // console.log('prevTime: ' + prevTime)
+            console.log('total: ' + totalTime)
+
+            
+            if(totalTime > limit){
                 break;
             }
         }
-
+        // 푸는 시간이 오버되면 level을 다시 설정
         if (totalTime > limit) {
             left = mid + 1;
-        } else {
+        } else { // 푸는 시간이 넉넉하므로 최솟값을 찾기 위해 level최댓값을 다시 수정
             right = mid;
         }
     }
@@ -32,4 +41,8 @@ function solution(diffs, times, limit) {
     return left;
 }
 
-console.log(solution([1, 4, 4, 2], [6, 3, 8, 2], 59));
+diffs = [1, 5, 3];
+times = [2, 4, 7];
+limit = 30;
+
+console.log(solution(diffs,times,limit));
